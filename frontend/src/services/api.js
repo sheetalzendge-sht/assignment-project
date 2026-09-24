7f5149105c45b53c0413929892600f1c3c5fb921
Sheetal Zendge <sheetalzendge@gmail.com>
2026-09-24 23:05:11 +0530

    Fix frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
   baseURL: "https://assignment-project-production-4b96.up.railway.app",
   headers: {
     "Content-Type": "application/json",
